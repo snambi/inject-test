26a43464a7b0c35529fab1d8cfe0dca42b3cae8a
Nambi Sankaran <snambi@gmail.com>
2013-09-24 21:13:39 -0700

    added a generated test case

---
+++ spring/src/test/java/org/github/spring/NotifierServiceImplTest.java
@@ -0,0 +1,27 @@
+package org.github.spring;
+
+import org.junit.Test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: nsankaran
+ * Date: 9/24/13
+ * Time: 9:08 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class NotifierServiceImplTest {
+    @Test
+    public void testStartup() throws Exception {
+
+    }
+
+    @Test
+    public void testShutdown() throws Exception {
+
+    }
+
+    @Test
+    public void testNotify() throws Exception {
+
+    }
+}
